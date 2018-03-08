0af64efc18b53a5560f1121e959cb4f3f23bf714
Melissa Utomo <mafutomo@gmail.com>
2018-03-07 22:13:45 -0700

    added signup component

---
+++ epro/src/views/signup.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Container, Header, Footer, Content } from 'native-base';
+
+class SignUp extends Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+
+       }
+  }
+
+  render() {
+    return (
+        <Container>
+          <Head />
+          <Content>
+
+          </Content>
+          <Footer>
+          </Footer>
+        </Container>
+      )
+  }
+};
+
+
+export default SignUp;
