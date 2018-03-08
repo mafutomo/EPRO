35e7ebfd0b30d8f279c400d6841a60c1fc76ab77
Stephanie Marvez <marvez@gmail.com>
2018-03-07 21:18:58 -0700

    started bottomnav

--- epro/src/components/bottomnav.js
+++ epro/src/components/bottomnav.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Container, Footer, FooterTab, Button, Icon, StyleProvider } from 'native-base';
 export default class BottomNav extends Component {
   render() {
     return (
-      <Container>
-        <Footer>
-          <FooterTab>
-            <Button>
-              <Icon name="apps" />
-            </Button>
-            <Button>
-              <Icon name="camera" />
-            </Button>
-            <Button active>
-              <Icon active name="navigate" />
-            </Button>
-            <Button>
-              <Icon name="person" />
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+        <Container style={styles.container}>
+          <Footer>
+            <FooterTab>
+              <Button active tyle={styles.container}>
+                <Icon active name="home" />
+              </Button>
+              <Button>
+                <Icon name="calendar" />
+              </Button>
+              <Button>
+                <Icon name="stats" />
+              </Button>
+              <Button>
+                <Icon name="person" />
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'green',
+    },
+    textContent: {
+      fontSize: 20,
+      color: 'red',
+    },
+  });
