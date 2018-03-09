ae086223355195d4ffbb9a31fc2ee548d1151a48
Stephanie Marvez <marvez@gmail.com>
2018-03-09 10:14:14 -0700

    date picker/dropdown created

--- epro/src/components/dropdown.js
+++ epro/src/components/dropdown.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import { StyleSheet, Picker } from 'react-native';
-import { View } from 'native-base';
-export default class Dropdown extends Component {
+import { View, Container } from 'native-base';
+import { Dropdown } from 'react-native-material-dropdown';
+export default class DatePicker extends Component {
   render() {
+    let data = [{
+      value: 'Past 30 days',
+    }, {
+      value: 'Past 3 months',
+    }, {
+      value: 'Past 6 months',
+    }];
+
     return (
-      <View>
-        <Picker>
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
-        </Picker>
-      </View>
+        <Dropdown
+          label='Date Range'
+          data={data}
+          dropdownMargins={{min: 10, max: 16}}
+          containerStyle={styles.container}
+          itemTextStyle={styles.text}
+        />
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+
+  },
+  text: {
+    // textAlign: 'center',
+    fontFamily: 'DidactGothic-Regular',
+  }
+})
--- epro/src/views/home.js
+++ epro/src/views/home.js
@@ -11,7 +11,7 @@ import TopNav from '../components/topnav';
 import BottomNav from '../components/bottomnav';
 import ExerciseDetail from '../components/exercisedetail';
 import HistoryTable from '../components/historytable';
-import Dropdown from '../components/dropdown';
+import DatePicker from '../components/dropdown';
 
 class Home extends Component {
   constructor(props) {
@@ -28,7 +28,7 @@ class Home extends Component {
           {/* <Banner /> */}
           <Content>
             {/* <ExerciseDetail /> */}
-            <Dropdown />
+            <DatePicker />
             <HistoryTable />
               {/* <SmallInputBox />
               <InputBox />
