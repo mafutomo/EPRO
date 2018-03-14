61be984c0981ac2ceb10fe4b6b456048ef3ff04a
Melissa Utomo <mafutomo@gmail.com>
2018-03-14 17:05:06 -0600

    updated calendar logic for tabs

--- epro/src/components/calendarnav.js
+++ epro/src/components/calendarnav.js
@@ -16,11 +16,13 @@ class CalendarNav extends Component {
      super(props)
      this.state = {
        exercises:[],
+       dateTabs:[],
+       currentDate: new Date()
        }
   }
 
-
   async componentWillMount(){
+    console.log("get to days in month => ",this.getDaysInMonth())
     const response = await fetch('https://epro-fitness-api.herokuapp.com/users/2/workouts/03-05-18', {
       method: 'GET',
       headers: {
@@ -29,21 +31,55 @@ class CalendarNav extends Component {
       }
     })
     const responseJson = await response.json()
+    console.log("RESPONSEJSON",responseJson);
     this.setState({exercises: responseJson[0].exercises})
+
     setTimeout(this._tabs.goToPage.bind(this._tabs,2))
   }
 
-  renderExercises(){
+  getDaysInMonth () {
+    let currentDate = new Date()
+    let currentMonth = currentDate.getMonth()
+    let currentYear = currentDate.getFullYear()
+
+     let date = new Date(currentYear, currentMonth, 1)
+     let days = []
+     while (date.getMonth() === currentMonth) {
+       let dateElement = new Date(date)
+        days.push(dateElement.toISOString().slice(0,10))
+        date.setDate(date.getDate() + 1)
+     }
+     this.setState({dateTabs:days})
+     return days
+   }
+
+  renderTabs(){
+    return this.state.dateTabs.map(tab => {
+      let tabName = tab.substring(5,10)
+      return <Tab
+              tabStyle={{backgroundColor: '#17252A'}}
+              activeTabStyle={{backgroundColor: '#17252A'}}
+              activeTextStyle={{color: '#DEF2F1'}}
+              heading={`${tabName}`}
+              style = {styles.tabBody}>
+              </Tab>
+    })
+  }
+
+  renderExercises() {
     return this.state.exercises.map( exercise => {
       return <ExerciseDetail
       key = {exercise.exercise_id}
       exerciseName= {exercise.name}
       data = {[[exercise.sets,exercise.reps,exercise.weight,exercise.time]]}/>
     })
+
   }
 
   render() {
     console.log("state of exercises", this.state.exercises);
+    console.log("this.state.dateTabs", this.state.dateTabs);
+    console.log("this.state.currentDate", this.state.currentDate.toISOString().split('T'));
     return (
       <Container>
         <Header hasTabs
@@ -54,49 +90,7 @@ class CalendarNav extends Component {
         renderTabBar={()=> <ScrollableTab tabsContainerStyle={{color: '#DEF2F1'}}/>}
         ref={component => this._tabs = component}
         >
-          <Tab
-          tabStyle={{backgroundColor: '#17252A'}}
-          activeTabStyle={{backgroundColor: '#17252A'}}
-          activeTextStyle={{color: '#DEF2F1'}}
-          heading="Mon 12"
-          style = {styles.tabBody}>
-
-          {this.renderExercises()}
-
-          </Tab>
-          <Tab
-          tabStyle={{backgroundColor: '#17252A'}}
-          activeTabStyle={{backgroundColor: '#17252A'}}
-          activeTextStyle={{color: '#DEF2F1'}}
-          heading="Tues 13"
-          style = {styles.tabBody}>
-            <ExerciseDetail />
-            <ExerciseDetail />
-            <ExerciseDetail />
-            <ExerciseDetail />
-          </Tab>
-          <Tab
-          tabStyle={{backgroundColor: '#17252A'}}
-          activeTabStyle={{backgroundColor: '#17252A'}}
-          activeTextStyle={{color: '#DEF2F1'}}
-          heading="Wed 14">
-            <SmallInputBox />
-          </Tab>
-          <Tab
-          tabStyle={{backgroundColor: '#17252A'}}
-          activeTabStyle={{backgroundColor: '#17252A'}}
-          activeTextStyle={{color: '#DEF2F1'}}
-          heading="Thurs 15"
-          >
-            <InputBox />
-          </Tab>
-          <Tab
-          tabStyle={{backgroundColor: '#17252A'}}
-          activeTabStyle={{backgroundColor: '#17252A'}}
-          activeTextStyle={{color: '#DEF2F1'}}
-          heading="Fri 16">
-
-          </Tab>
+          {this.renderTabs()}
         </Tabs>
       </Container>
     );
