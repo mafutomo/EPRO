076cde90e472f565d8a0903a782638ce0a88be8e
Melissa Utomo <mafutomo@gmail.com>
2018-03-14 23:34:58 -0600

    created logic to fetch exercises for user chosen tab

--- epro/src/components/calendarnav.js
+++ epro/src/components/calendarnav.js
@@ -15,18 +15,24 @@ class CalendarNav extends Component {
   constructor(props) {
      super(props)
      this.state = {
-       exercises:[],
+       todayDate: new Date(),
+       todayISODate:'',
+       usersCurrentTab:'',
        dateTabs:[],
-       currentDate: new Date()
+       exercises:[],
        }
   }
 //'https://epro-fitness-api.herokuapp.com/users/2/workouts/03-05-18'
   async componentWillMount(){
-    this.getDaysInMonth()
-    let currentDate = new Date()
-    let currentTab = parseInt(currentDate.toISOString().slice(8,10))-1
 
-    const response = await fetch('https://localhost:3001/users/2/workouts/03-05-18', {
+    //create the array of dates
+    this.getDateTabsArray()
+
+    //create the value to fetch for exercises on current date
+    let currentISODate = this.state.todayDate.toISOString().split('T')[0]
+    this.setState({todayISODate:currentISODate})
+
+    const response = await fetch(`http://localhost:3001/users/1/workouts/${currentISODate}`, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
@@ -35,15 +41,18 @@ class CalendarNav extends Component {
     })
     const responseJson = await response.json()
 
+    //set execises state with today's workouts
     this.setState({exercises: responseJson[0].exercises})
 
-    setTimeout(this._tabs.goToPage.bind(this._tabs,currentTab))
+    //have tab automatically navigate to the current date
+    let currentDayIndex = parseInt(this.state.todayDate.toISOString().slice(8,10))-1
+    setTimeout(this._tabs.goToPage.bind(this._tabs,currentDayIndex))
   }
 
-  getDaysInMonth () {
-    let currentDate = new Date()
-    let currentMonth = currentDate.getMonth()
-    let currentYear = currentDate.getFullYear()
+//to create an array of all the days of current month
+  getDateTabsArray () {
+    let currentMonth = this.state.todayDate.getMonth()
+    let currentYear = this.state.todayDate.getFullYear()
 
      let date = new Date(currentYear, currentMonth, 1)
      let days = []
@@ -56,21 +65,42 @@ class CalendarNav extends Component {
      return days
    }
 
+//to render tabs showing all dates in the dateTabs array as individual tabs
   renderTabs(){
     return this.state.dateTabs.map(tab => {
+
       let stringTab = tab.toString()
       let tabName = stringTab.substr(0, 10)
-      return <Tab
-              tabStyle={{backgroundColor: '#17252A'}}
-              activeTabStyle={{backgroundColor: '#17252A'}}
-              activeTextStyle={{color: '#DEF2F1'}}
-              heading={`${tabName}`}
-              style = {styles.tabBody}>
 
-              </Tab>
+      if(tab.toISOString().split('T')[0] == this.state.todayISODate){
+        
+        //if it is the current date, then render the exercises in this tab
+        return <Tab
+                tabStyle={{backgroundColor: '#17252A'}}
+                activeTabStyle={{backgroundColor: '#17252A'}}
+                activeTextStyle={{color: '#DEF2F1'}}
+                heading={`${tabName}`}
+                style = {styles.tabBody}>
+
+                {this.renderExercises()}
+
+                </Tab>
+      } else {
+
+        //if not, do not render any exercises in this tab
+        return <Tab
+                tabStyle={{backgroundColor: '#17252A'}}
+                activeTabStyle={{backgroundColor: '#17252A'}}
+                activeTextStyle={{color: '#DEF2F1'}}
+                heading={`${tabName}`}
+                style = {styles.tabBody}>
+
+                </Tab>
+      }
     })
   }
 
+//render the exercise cards based on the this.state.exercises
   renderExercises() {
     return this.state.exercises.map( exercise => {
       return <ExerciseDetail
@@ -78,13 +108,26 @@ class CalendarNav extends Component {
       exerciseName= {exercise.name}
       data = {[[exercise.sets,exercise.reps,exercise.weight,exercise.time]]}/>
     })
+  }
 
+//initiated by onChangeTab, this updates the state tracking date user is currently viewing
+  setCurrentTabState(i) {
+    console.log(i);
+    let newState = `${this.state.todayDate.getMonth()+1}-${i.i+1}-${this.state.todayDate.getFullYear()}`
+    console.log("newState == ",newState);
+    this.setState({usersCurrentTab:newState})
+    if(newState) {
+      fetch(`http://localhost:3001/users/1/workouts/${this.state.usersCurrentTab}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(responseJson => {
+        console.log("response == ", responseJson);
+      })
+    }
   }
 
   render() {
-    console.log("state of exercises", this.state.exercises);
-    console.log("this.state.dateTabs", this.state.dateTabs);
-    console.log("this.state.currentDate", this.state.currentDate.toISOString().split('T'));
     return (
       <Container>
         <Header hasTabs
@@ -94,6 +137,7 @@ class CalendarNav extends Component {
         tabBarUnderlineStyle = {{backgroundColor: '#CB2D6F'}}
         renderTabBar={()=> <ScrollableTab tabsContainerStyle={{color: '#DEF2F1'}}/>}
         ref={component => this._tabs = component}
+        onChangeTab={(i) => {this.setCurrentTabState(i)}}
         >
           {this.renderTabs()}
         </Tabs>
