11880b634d7a98fa392aae3e18865ad14afc0e01
Stephanie Marvez <marvez@gmail.com>
2018-03-16 11:55:22 -0600

    hormone chart looking better

--- epro/src/components/hormonechart.js
+++ epro/src/components/hormonechart.js
@@ -161,9 +161,9 @@ class HormoneChart extends Component {
           return path;
       }
 
-      createBarChart(x, y0, y1, w, h) {
+      createBarChart(x, y, w, h) {
           var path = d3.path.path();
-          path.rect(x, y0, y1, w, h);
+          path.rect(x, y, w, h);
           return path;
       }
 
@@ -195,7 +195,7 @@ class HormoneChart extends Component {
 
     const y1 = d3.scale.scaleLinear()
             .rangeRound([height, 0])
-            .domain([0, maxProgesterone])
+            .domain([0, maxProgesterone-1])
 
     const firstDay = x(data[0].day)
     const secondDay = x(data[1].day)
@@ -209,14 +209,14 @@ class HormoneChart extends Component {
                             .y(() => 0)
                             (bottomAxis)
 
-    const leftAxis = ticks(0, maxEstrogen+25, 5)
+    const leftAxis = ticks(0, maxEstrogen, 5)
 
     const leftAxisD = d3.shape.line()
                         .x(() => bottomAxis[0] + labelDx)
                         .y(d => y0(d) - height)
                         (leftAxis)
 
-    const rightAxis = ticks(0, maxProgesterone, 5)
+    const rightAxis = ticks(0, maxProgesterone, 6)
 
     const rightAxisD = d3.shape.line()
                         .x(() => bottomAxis[1] + labelDx)
@@ -227,11 +227,20 @@ class HormoneChart extends Component {
     const labelDistance = 9
     const labelDistanceR = 11
     const emptySpace = "";
-    const leftAxisTitle = new Transform().rotate(-90);
+    const hormones = ["estrogen", "progesterone"]
 
     return (
         <View>
         <Surface style={styles.container} width={screen.width} height={screen.height}>
+          <Group style={styles.legend}>
+            <Shape d={new Transform().rotate(-90[50,50])}/>
+              <Text
+                fill={colors.black}
+                x={50}
+                y={50}
+                font="12px helvetica" >
+              </Text>
+          </Group>
           <Group x={margin.left} y={margin.top}>
                     <Group x={0} y={height}>
                         <Group key={-1}>
@@ -259,14 +268,15 @@ class HormoneChart extends Component {
                             <Shape stroke={colors.black} d={leftAxisD} key="-1"/>
                             {
                                 leftAxis.map((d, i) => (
-                                    <Group x={5} y={y0(d)-height} key={i + 1}>
+                                    <Group x={-1} y={y0(d)-height} key={i + 1}>
                                         <Shape d={this.drawLine(notch, 0)} stroke={colors.black}/>
                                         <Text
                                             fill={colors.black}
-                                            x={-25}
+                                            x={-5}
                                             y={-labelDistance}
                                             font="12px helvetica"
-                                            // style={styles.yAxisText}
+                                            alignment="right"
+
                                         >
                                             {d + emptySpace}
                                         </Text>
@@ -275,34 +285,17 @@ class HormoneChart extends Component {
                             }
                         </Group>
                         <Group key={-3} >
-                          <Text
-                              style={styles.textStyle}
-                              x={0}
-                              y={-height/2}
-                              font = {{
-                                 fontFamily:'Helvetica, Neue Helvetica, Arial',
-                                 fontSize:13,
-                                 fontWeight:"normal", // or "normal"
-                                 fontStyle:"normal" // or "normal"
-                               }}
-                               rotate={"45"}
-                               textAnchor={"middle"}
-                               fill = "#000000"
-                              // alignment = { leftAxisTitle }
-                            >
-                              Hello World
-                            </Text>
                             <Shape stroke={colors.black} d={rightAxisD} key="-1"/>
                             {
                                 rightAxis.map((d, i) => (
-                                    <Group x={width-10} y={y1(d)-height} key={i + 1}>
+                                    <Group x={width-3} y={y1(d)-height} key={i + 1}>
                                         <Shape d={this.drawLine(notch, 0)} stroke={colors.black}/>
                                         <Text
                                             fill={colors.black}
-                                            x={15}
-                                            y={-labelDistanceR}
+                                            x={10}
+                                            y={-labelDistance}
                                             font="12px helvetica"
-                                            alignment="center"
+                                            alignment="left"
                                         >
                                             {d + emptySpace}
                                         </Text>
@@ -336,6 +329,9 @@ class HormoneChart extends Component {
                 </Group>
 
           </Surface>
+          {/* <View style={styles.legend}>
+            <Text>Hello</Text>
+          </View> */}
         </View>
       )
   }
@@ -353,7 +349,13 @@ const styles = StyleSheet.create({
     fontWeight: 'normal',
   },
   textStyle: {
-    transform: [{ rotate: '-90deg' }]
+    transform: [{ rotate: '90deg'}],
+  },
+  legend: {
+    backgroundColor: 'pink',
+    position: 'absolute',
+    height: 50,
+    width: 50
   }
 });
 
