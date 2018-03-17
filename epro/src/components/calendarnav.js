0a0a8605a0a0cd5c72eb5d63cb427eec3758b61f
Melissa Utomo <mafutomo@gmail.com>
2018-03-16 21:39:57 -0600

    fixed scrolling issue

--- epro/src/components/calendarnav.js
+++ epro/src/components/calendarnav.js
@@ -20,10 +20,18 @@ class CalendarNav extends Component {
        dateTabs:[],
        selectedExercises:[],
        isEditModalVisible: false,
+       inputExerciseName:'',
+       inputDescription:"",
+       inputRep:0,
+       inputSet:0,
+       inputWeight:0,
+       inputTime:'00:00',
+       // initialPage: 1,
+       // activeTab: 1,
        }
   }
 //'https://epro-fitness-api.herokuapp.com/users/2/workouts/03-05-18'
-  async componentWillMount(){
+  async componentDidMount(){
 
     //create the array of dates
     this.getDateTabsArray()
@@ -31,6 +39,16 @@ class CalendarNav extends Component {
     //create the value to fetch for exercises on current date
     let currentISODate = this.state.todayDate.toISOString().split('T')[0]
     this.setState({todayISODate:currentISODate})
+    console.log('currentISO date ==', currentISODate);
+
+    //have tab automatically navigate to the current date
+    let currentDayTabIndex = parseInt(this.state.todayDate.toISOString().slice(8,10))-1
+    console.log("currentDayTabIndex==",currentDayTabIndex)
+    setTimeout(this._tabs.goToPage.bind(this._tabs,currentDayTabIndex))
+    // this.setState({
+    //   initialPage:currentDayTabIndex,
+    //   activeTab:currentDayTabIndex,
+    // })
 
     const response = await fetch(`http://localhost:3001/users/1/workouts/${currentISODate}`, {
       method: 'GET',
@@ -40,13 +58,10 @@ class CalendarNav extends Component {
       }
     })
     const responseJson = await response.json()
-
     //set execises state with today's workouts
     this.setState({selectedExercises: responseJson[0].exercises})
 
-    //have tab automatically navigate to the current date
-    let currentDayTabIndex = parseInt(this.state.todayDate.toISOString().slice(8,10))-1
-    setTimeout(this._tabs.goToPage.bind(this._tabs,currentDayTabIndex))
+
   }
 
 //to create an array of all the days of current month
@@ -85,7 +100,7 @@ class CalendarNav extends Component {
                 style = {styles.tabBody}>
                   {this.renderExercises()}
                 </Tab>
-                
+
           } else {
 
             return <Tab
@@ -93,18 +108,8 @@ class CalendarNav extends Component {
                     activeTabStyle={{backgroundColor: '#17252A'}}
                     activeTextStyle={{color: '#DEF2F1'}}
                     heading={`${tabName}`}
-                    style = {styles.tabBody}
+                    style = {styles.tabEmpty}
                     >
-
-                    <TouchableOpacity
-                    style={styles.iconContainer}
-                    onPress={()=>{this.toggleEditModalVisible()}}>
-                      <Icon
-                        active name="add-circle"
-                        size={45}
-                        color={'#FFBA49'}
-                      />
-                    </TouchableOpacity>
 
                     </Tab>
           }
@@ -153,27 +158,42 @@ class CalendarNav extends Component {
     this.setState({ isEditModalVisible: !this.state.isEditModalVisible })
   }
 
+  hideMondalOmitState = () => {
+    this.setState({
+      inputExerciseName:"",
+      inputDescription:"",
+      inputRep:0,
+      inputWeight:0,
+      inputSet:0,
+      inputTime:'00:00',
+      isEditModalVisible: false
+    })
+
+  }
+
 //for creating a new exercise in the current tab
   addExercise = () => {
-    fetch(`http://localhost:3001/users/1/workouts/03-16-2018`,{
+    let currentTabDate = this.state.usersCurrentTab
+    fetch(`http://localhost:3001/users/1/workouts/${currentTabDate}`,{
       method:'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
       body : JSON.stringify({
-        name:"Pull Ups",
-        sets:1,
-        reps:3,
-        weight:0,
-        time:"01:00",
+        name:this.state.inputExerciseName,
+        description:this.state.inputDescription,
+        sets:this.state.inputSet,
+        reps:this.state.inputRep,
+        weight:this.state.inputWeight,
+        time:this.state.inputTime,
       })
     })
     .then( response => {
       return response.json()
     })
     .then(responseJson => {
-      console.log(responseJson[0]);
+      console.log(responseJson[0])
       var newState = this.state.selectedExercises.slice()
       newState.push(responseJson[0])
       this.setState({ selectedExercises: newState })
@@ -182,7 +202,7 @@ class CalendarNav extends Component {
   }
 
   render() {
-    console.log(this.state.selectedExercises);
+
     return (
       <Container>
         <Header hasTabs
@@ -192,12 +212,23 @@ class CalendarNav extends Component {
         tabBarUnderlineStyle = {{backgroundColor: '#CB2D6F'}}
         renderTabBar={()=> <ScrollableTab tabsContainerStyle={{color: '#DEF2F1'}}/>}
         ref={component => this._tabs = component}
-        onChangeTab={(i) => {this.setCurrentTabState(i)}}
+        // initialPage={this.state.initialPage} page={this.state.activeTab}
+        onChangeTab= {(i) => {this.setCurrentTabState(i)}}
         >
           {this.renderTabs()}
+
         </Tabs>
 
 
+        <TouchableOpacity
+        style={styles.iconContainer}
+        onPress={()=>{this.toggleEditModalVisible()}}>
+          <Icon
+            active name="add-circle"
+            size={45}
+            color={'#FFBA49'}
+          />
+        </TouchableOpacity>
 
         //Add Exercise Modal
           <Modal
@@ -209,38 +240,44 @@ class CalendarNav extends Component {
               <Text
               style={styles.modalTitle}>Create an Exercise</Text>
 
-            <InputBox/>
-            <InputBox/>
+            <InputBox
+            onChangeText={(text) => this.setState({inputExerciseName:text})}/>
+            <InputBox
+            onChangeText={(text) => this.setState({inputDescription:text})}/>
             <View style = {{flexDirection: 'row'}}>
             <View style = {{flexDirection:'column',alignItems:'center',paddingRight:15}}>
             <Text>Sets</Text>
-            <SmallInputBox/>
+            <SmallInputBox
+            onChangeText={(text) => this.setState({inputSet:text})}/>
             </View>
             <View style = {{flexDirection:'column',alignItems:'center'}}>
             <Text>Weight</Text>
-            <SmallInputBox/>
+            <SmallInputBox
+            onChangeText={(text) => this.setState({inputWeight:text})}/>
             </View>
             </View>
 
             <View style = {{flexDirection: 'row'}}>
             <View style = {{flexDirection:'column',alignItems:'center',paddingRight:15}}>
               <Text>Reps</Text>
-              <SmallInputBox/>
+              <SmallInputBox
+              onChangeText={(text) => this.setState({inputRep:text})}/>
             </View>
             <View style = {{flexDirection:'column',alignItems:'center'}}>
             <Text>Time</Text>
-            <SmallInputBox/>
+            <SmallInputBox
+            onChangeText={(text) => this.setState({inputTime:text})}/>
             </View>
             </View>
 
             <View style = {{flexDirection: 'row'}}>
-              <TouchableOpacity onPress={this.toggleEditModalVisible}>
+              <TouchableOpacity onPress = {this.hideMondalOmitState}>
                 <Text
-                onPress = {() => this.setState({ isEditModalVisible: false })}
+
                 >Cancel</Text>
               </TouchableOpacity>
 
-              <TouchableOpacity onPress={this.toggleEditModalVisible}>
+              <TouchableOpacity>
                 <Text
                 onPress = {
                   this.addExercise
@@ -331,6 +368,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     height: 500,
   },
+  tabEmpty:{
+
+  }
 
 });
 
