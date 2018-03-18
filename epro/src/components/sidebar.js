efe7e15c91dca333609af77d9495b32a9279d455
Melissa Utomo <mafutomo@gmail.com>
2018-03-18 17:06:21 -0600

    updated nav bar

--- epro/src/components/sidebar.js
+++ epro/src/components/sidebar.js
@@ -1,41 +1,60 @@
 import React from "react";
 import { AppRegistry, StyleSheet, Image, StatusBar, View } from "react-native";
-import { Container, Content, Text, List, ListItem } from "native-base";
+import { Container, Content, Text, List, ListItem, Header, Icon } from "native-base";
 const routes = ["ABOUT E/PRO", "LOGOUT"];
+const icons = ["information-circle", "exit"]
 
-export default class SideBar extends React.Component {
-  render() {
+const SideBar = (props) => {
+
     return (
       <Container>
+      <Header style = {styles.header}>
+        </Header>
         <Content style={styles.body}>
-
-          <List
-            dataArray={routes}
-            renderRow={data => {
-              return (
+          <List>
                 <ListItem
                   button
-                  onPress={() => this.props.navigation.navigate(data)}>
+                  style={{borderBottomWidth: 0}}
+                  onPress={() => this.props.navigation.navigate("About E/Pro")}>
+                  <Icon name='arrow-back'
+                  style ={styles.iconStyle} />
                   <Text
-                  style={styles.textStyle}>{data}</Text>
+                  style={styles.textStyle}>About E/Pro</Text>
                 </ListItem>
-              );
-            }}
-          />
+
+                <ListItem
+                  button
+                  style={{borderBottomWidth: 0}}
+                  onPress={() => this.props.navigation.navigate("Logout")}>
+                  <Icon name='arrow-back'
+                  style ={styles.iconStyle} />
+                  <Text
+                  style={styles.textStyle}>Log out</Text>
+                </ListItem>
+          </List>
         </Content>
       </Container>
     );
-  }
+
 }
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: '#3AAFA9',
+    backgroundColor: '#FEFFFF',
 
   },
   textStyle: {
     fontFamily:'DidactGothic-Regular',
-    color: '#FEFFFF',
-    fontSize:15,
+    color: '#17252A',
+    fontSize:20,
+  },
+  header: {
+    height: 130,
+    backgroundColor: '#DEF2F1',
+  },
+  iconStyle:{
+
   }
 })
+
+export default SideBar;
